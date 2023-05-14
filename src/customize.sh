89fb9369ceba991469c6d4a6edb9d16f6b3db15f
nivranaitsirhc <naitsirhc.uriel@gmail.com>
2023-05-14 23:11:06 +0800

    [fix] fixed bug about install error due to incomplete removal code pertaining binaries

--- src/customize.sh
+++ src/customize.sh
@@ -4,11 +4,6 @@
 
 [ ! "$BOOTMODE" ]                   && abort "Install only via Magisk Manager"
 [ "$MAGISK_VER_CODE" -lt "25000" ]  && abort "Requires Magisk v25 and Above"
-[ ! -f "$MODPATH/aapt.zip" ]        && abort "Missing included bin files"
-
-unzip -o -q -d "$TMPDIR/aapt" "$MODPATH/aapt.zip" 
-
-[ ! -d "$TMPDIR/aapt" ]             && abort "Unable to unzip bin files"
 
 target_bin_arch=arm
 case "$ARCH" in
@@ -25,29 +20,26 @@ case "$ARCH" in
     target_bin_arch=i686
     ;;
     *)
-    abort "Unknown archeticture $ARCH"
+    abort "* Unknown archeticture $ARCH"
     ;;
 esac
-ui_print "* arch type - $target_bin_arch."
+ui_print "* arch type is $target_bin_arch."
 
-
-path_bin_dir="$TMPDIR/aapt/aapt/$target_bin_arch"
 wgetURI="https://github.com/nivranaitsirhc/termux-aapt/raw/main/prebuilt-binary/${target_bin_arch}/aapt2"
-[ "$API" -ge "31" ] && wgetURI="https://github.com/nivranaitsirhc/termux-aapt/raw/main/prebuilt-binary-android-12%2B/${target_bin_arch}/aapt2"
+[ "$API" -ge "31" ] && \
+wgetURI="https://github.com/nivranaitsirhc/termux-aapt/raw/main/prebuilt-binary-android-12%2B/${target_bin_arch}/aapt2"
 
-ui_print "* downloading binary files.."
-wget "$wgetURI" -O "$MODPATH/bin/aapt2"
+ui_print "* downloading $ARCH binaries"
+wget "$wgetURI" -O "$MODPATH/bin/aapt2" || abort " failed to download $ARCH binaries"
 
 if [ -f "$MODPATH/bin/aapt2" ];then
     chmod +x "$MODPATH/bin/aapt2"
     "$MODPATH/bin/aapt2" version || {
-        ui_print "* binary file is currupted"
-        abort " downloaded binary file is currupted"
+        abort "* downloaded binary file is currupted"
     }
-    ui_print "* successfully downloaded binary file"
 else
     ui_print "* failed to download binary file."
-    abort "please check your internet connection and try again."
+    abort "* please check your internet connection and try again."
 fi
 
 sdcard_folder="/sdcard/DynamicMountManagerX"
@@ -55,6 +47,8 @@ sdcard_folder="/sdcard/DynamicMountManagerX"
 [ ! -d "$sdcard_folder" ] && {
     ui_print "* setting up sdcard folder"
     mkdir -p "$sdcard_folder/apps"
+    touch "$sdcard_folder/enable"
+    touch "$sdcard_folder/debug"
 }
 
 # set permissions
@@ -62,7 +56,11 @@ ui_print "* setting permissions."
 set_perm_recursive "$MODPATH/bin"   root root 0755 0655 u:object_r:magisk_file:s0
 set_perm_recursive "$MODPATH/lib"   root root 0755 0644 u:object_r:magisk_file:s0
 
-# check for dynmount
-[ ! -d "/data/adb/modules/magisk_proc_monitor" ] && {
-    ui_print "* missing Magisk Process Monitor. Please install it after this module."
+# check for proccess monitor
+[ ! -d "$MAGISKTMP/.magisk/modules/magisk_proc_monitor" ] && {
+    MPMURL=http://github.com/HuskyDG/magisk_proc_monitor
+    ui_print "* process monitor tool is not installed"
+    ui_print "* please install it from $MPMURL"
+    sleep 3
+    am start -a android.intent.action.VIEW -d "$MPMURL" &>/dev/null
 }
