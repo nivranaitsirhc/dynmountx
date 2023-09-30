c35915e959ebfd4048d3bbe554a3ed4875b07317
nivranaitsirhc <naitsirhc.uriel@gmail.com>
2023-09-30 11:20:46 -0700

    [core] re-work service.sh

--- src/service.sh
+++ src/service.sh
@@ -5,66 +5,121 @@
 # Wait until boot is completed
 until [ "$(getprop sys.boot_completed)" = 1 ];do sleep 1;done
 
-# magisk_module required
+# magisk module variables
 MODDIR="${0%/*}"
-# MODNAME="${MODDIR##*/}"
-
+MODNAME="${MODDIR##*/}"
 MAGISKTMP=$(magisk --path) || MAGISKTMP=/sbin
 
-MIRROR="$MAGISKTMP/.magisk/mirror"
+export MODDIR
+export MODNAME
+export MAGISKTMP
 
-# disable manager.sh
+# disable manager.sh execution from dynmount.sh
 echo 0 > "$MODDIR/bootscript"
 
-# logger library required variables
+# manager.sh required variables
 # -----------------------
-[[ -v STAGE ]]  || export STAGE=boot-service
-# [[ -v PROC ]]   || export PROC=magisk
-[[ -v UID ]]    || { UID=$(id -g) && export UID; }
-[[ -v PID ]]    || export PID=$$
+[[ -v STAGE ]]  || STAGE=boot-service
+# [[ -v PROC ]]   || PROC=magisk
+[[ -v UID ]]    || UID=$(id -g)
+[[ -v PID ]]    || PID=$$
+
+export STAGE
+export UID
+export PID
 
 
+path_dir_storage="/sdcard/DynamicMountManagerX"
+path_dir_apps_module="$MODDIR/apps"
+path_dir_apps_storage="$path_dir_storage/apps"
 
-# logger dummy function
+
+# instance log name
+log_instance_name="dynmount-$(date +%y-%m-%d_%H-%M-%S).log"
+# temp location to store log will be cache
+path_file_logs="/cache/$log_instance_name"
+
+# redirect final log to sdcard log file else to cache log file
+if [ -d "$path_dir_storage" ];then
+    path_file_logs_final="$path_dir_storage/module.log"
+else
+    path_file_logs_final="/cache/dynmount-module.log"
+fi
+export path_file_logs
+export path_file_logs_final
+
+
+# dummy logger function
 logme(){ :; }
-
-# source logger from lib
+# source libraries
 [ -d "$MODDIR/lib" ] && {
-    # logger
+    # logger library
     [ -f "$MODDIR/lib/logger.sh" ] && . "$MODDIR/lib/logger.sh"
 }
-
-# logger configs
+# customize logger data
 logger_process=$(printf "%-6s %-6s" "$UID" "$PID")
 logger_special=$(printf "%-18s - %s" "$(basename "$0"):$STAGE" "$PROC")
-# logger
-logger_check(){
-    if [[ -v LOGGER_MODULE ]] && [ -f "$path_dir_storage/debug" ]; then
-        [ -f "$path_file_logs" ] && {
-            cat "$path_file_logs" >> "$path_dir_storage/module.log"
-            printf "" > "$path_file_logs"
-        }
-    fi
+
+# copy final log from instance to final log final
+logger_check() {
+    # check if path_file_logs is still present
+    [ -f "$path_file_logs" ] && [ -f "$path_file_logs_final" ] && {
+        # copy instance log to final log destination
+        cat "$path_file_logs" >> "$path_file_logs_final"
+        # remove instance log
+        rm -f "$path_file_logs"
+    }
+}
+
+# send notifications
+send_notification() {
+    su 2000 -c "cmd notification post -S bigtext -t 'DynMountX' 'Tag' '$(printf "$1")'"
 }
 
 
-# itirate the /data/adb/app
-# run manager.sh with disabled restart me
+
+# query application directory
 [ ! -d "/data/adb/apps" ] && {
-    logme stats "exiting.. unable to find /data/adb/apps"
+    logme error "exiting.. missing applications at /data/adb/apps"
     logger_check
     return 1
 }
 
+
+# SC2031
+if shopt -s lastpipe;then
+    logme debug "shopt did not work."
+fi
+
+mountedAppList=""
+
+
+# query applications folder list and validate
 ls "/data/adb/apps" | while read -r PROC; do
     if [ ! -f "/data/adb/apps/$PROC/disable" ];then
+        # export current PROC
         export PROC
-        "$MODDIR/manager.sh" disableRestart
+        logme stats "loop(): calling manager.sh"
+        # call manager.sh
+        su 0 -c "$MODDIR/manager.sh" disableRestart disableNotificaitons
+
+        # verify mountpoint
+        if mount | grep -q "$PROC";then
+            mountedAppList="$mountedAppList\n$PROC"
+        fi
     else
         logme stats "loop(): skipping $PROC, disable tag found"
     fi
 done
-
 logme stats "done boot-script"
+
+# remove bootscript tag
 rm -f "$MODDIR/bootscript"
+
+# send notificatons to user
+if [ "$mountedAppList" != "" ];then
+    send_notification "Successfully Mounted$mountedAppList"
+fi
+
+# exit
 logger_check
